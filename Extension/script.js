5b7af7b32b629cc288757607e4580c5941dee53b
Yannick Weiss <contact@yannickweiss.com>
2018-10-21 20:30:51 +0200

    The Safari Extension.

--- Extension/script.js
+++ Extension/script.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    safari.extension.dispatchMessage("Hello World!");
+    function handleBackspace(e) {
+        if (e.keyCode === 8 && !e.ctrlKey && !e.shiftKey
+            && e.target.tagName !== 'INPUT' && e.target.tagName !== 'TEXTAREA') {
+            e.preventDefault();
+            window.history.go(-1);
+        }
+    }
+    window.addEventListener('keydown', handleBackspace, false);
 });
